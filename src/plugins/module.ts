dd5f6532d3e58ebd2fb2d8fc1e5c684f360f9796
goodmind <andwebar@gmail.com>
2017-11-19 17:41:32 +0500

    refactor(plugins): export id plugin

--- src/plugins/module.ts
+++ src/plugins/module.ts
@@ -2,8 +2,10 @@ import { provide } from 'core/di/provider';
 
 import { HelloPlugin } from './src/hello';
 import { HelpPlugin } from './src/help';
+import { IdPlugin } from './src/id';
 
 export const pluginBindings = [
   provide(HelloPlugin),
   provide(HelpPlugin),
+  provide(IdPlugin)
 ];
