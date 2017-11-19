5b92bc953eb31e60c1e5815ff458f59c59a947b3
goodmind <andwebar@gmail.com>
2017-11-19 18:31:09 +0500

    refactor(plugins/iq_test): fix lint problems

--- src/plugins/src/iq_test.ts
+++ src/plugins/src/iq_test.ts
@@ -1,33 +1,32 @@
-import { Plugin } from "core/bot_api/plugin";
-import { Inject } from "core/di/injector";
-import { Input } from "core/bot_api/input";
-import { TgApi } from "core/tg/tg_api";
-import { Message, User } from "node-telegram-bot-api";
-import Xor4k from "vendor/xor4096";
-
-const fullName = (user: User) =>
-  user.last_name
-    ? `${user.first_name} ${user.last_name}`
-    : `${user.first_name}`;
+import { Input } from 'core/bot_api/input';
+import { Plugin } from 'core/bot_api/plugin';
+import { Inject } from 'core/di/injector';
+import { fullName } from 'core/tg/message_util';
+import { TgApi } from 'core/tg/tg_api';
+import { Message } from 'node-telegram-bot-api';
+import Xor4k from 'vendor/xor4096';
 
 const iq = (s: string) => {
-  const xorgen = new Xor4k(s + "sas" + new Date().toDateString());
+  const xorgen = new Xor4k(s + 'sas' + new Date().toDateString());
   const res = xorgen.double();
-  if (res < 0.5) return 30 + Math.round(res * 100);
-  else return 80 + Math.round(res * 100);
+  if (res < 0.5) {
+    return 30 + Math.round(res * 100);
+  } else {
+    return 80 + Math.round(res * 100);
+  }
 };
 
 @Inject
 export class IqTestPlugin implements Plugin {
-  readonly name = "IQ Test";
+  public readonly name = 'IQ Test';
 
-  constructor(private api: TgApi) {}
+  constructor(private api: TgApi) { }
 
-  init(input: Input) {
+  public init(input: Input) {
     input.onText(/\/iq/, this.onMessage);
   }
 
-  onMessage = (msg: Message) => {
+  public onMessage = (msg: Message) => {
     this.api.reply(msg, `Ваш IQ: ${iq(fullName(msg.from!))}`);
-  };
+  }
 }
