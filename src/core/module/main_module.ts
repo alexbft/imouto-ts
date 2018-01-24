9786a00a46e083623ca8f8bda7271ef653813fe1
andretshurotshka <andwebar@gmail.com>
2018-01-25 04:58:49 +0500

    style(core/module/main_module): sort imports

--- src/core/module/main_module.ts
+++ src/core/module/main_module.ts
@@ -1,11 +1,11 @@
+import { BotApi } from 'core/bot_api/bot_api';
+import * as config from 'core/config/module';
 import { provide } from 'core/di/provider';
 import { Environment } from 'core/environment/environment';
 import { ImoutoServer } from 'core/server/imouto_server';
-import { Web } from 'core/util/web';
 import { TgApi } from 'core/tg/tg_api';
 import { TgClient } from 'core/tg/tg_client';
-import { BotApi } from 'core/bot_api/bot_api';
-import * as config from 'core/config/module';
+import { Web } from 'core/util/web';
 
 const bindings = [
   provide(Environment),
