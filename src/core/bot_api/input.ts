5321929ccf79b35a2cc439679c97b74b7c77ae01
andretshurotshka <andwebar@gmail.com>
2018-01-25 04:55:14 +0500

    refactor(core/bot_api/input): add error handler

--- src/core/bot_api/input.ts
+++ src/core/bot_api/input.ts
@@ -1,10 +1,12 @@
+import { PromiseOr } from 'core/util/promises';
 import { EventEmitter } from 'events';
 import { Message } from 'node-telegram-bot-api';
 
-type MessageHandler = (msg: Message) => void;
+type MessageHandler = (msg: Message, match: RegExpExecArray) => PromiseOr<void>;
+type ErrorHandler = (msg: Message, err: Error) => any;
 
 export interface Input {
-  onText(regex: RegExp, handler: MessageHandler): void;
+  onText(regex: RegExp, handler: MessageHandler, errorHandler?: ErrorHandler): void;
 }
 
 export class InputImpl implements Input {
@@ -16,10 +18,15 @@ export class InputImpl implements Input {
     }
   }
 
-  onText(regex: RegExp, handler: MessageHandler): void {
-    this.eventEmitter.on('text', (msg) => {
-      if (regex.test(msg.text)) {
-        handler(msg);
+  onText(regex: RegExp, handler: MessageHandler, errorHandler: ErrorHandler = (_, e) => { throw e; }): void {
+    this.eventEmitter.on('text', async (msg: Message) => {
+      if (msg.text != null && regex.test(msg.text)) {
+        try {
+          await handler(msg, regex.exec(msg.text)!);
+        } catch (e) {
+          console.error(e);
+          errorHandler(msg, e);
+        }
       }
     });
   }
