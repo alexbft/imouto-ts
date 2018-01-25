e9c1b883da31c1ba14f7d698043566d18a685236
andretshurotshka <andwebar@gmail.com>
2018-01-25 05:01:52 +0500

    feat(plugins/translate): add translate plugin

---
+++ src/plugins/src/translate.ts
@@ -0,0 +1,94 @@
+import { Input } from 'core/bot_api/input';
+import { Plugin } from 'core/bot_api/plugin';
+import { Inject } from 'core/di/injector';
+import { TgApi } from 'core/tg/tg_api';
+import { Web } from 'core/util/web';
+import { Message } from 'node-telegram-bot-api';
+
+@Inject
+export class TranslatePlugin implements Plugin {
+  readonly name = 'Translate';
+
+  constructor(
+    private api: TgApi,
+    private web: Web,
+  ) {}
+
+  async translate(src: string, dest: string, txt: string) {
+    const res = await this.web.getAsBrowser('http://translate.google.com/translate_a/single', {
+      qs: {
+        client: 's',
+        ie: 'UTF-8',
+        oe: 'UTF-8',
+        sl: src,
+        tl: dest,
+        dt: 't',
+        q: txt,
+      },
+    });
+
+    try {
+      console.log(res);
+      const evalFn = new Function(`return ${res}`);
+      const json: string[][] = evalFn();
+      return json[0].map((d) => d[0]).join('');
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  init(input: Input) {
+    input.onText(
+      /!(переведи|translate|перевод|расшифруй|tr)( [a-z]{2})?( [a-z]{2})?(?: ([^]+))?$/,
+      this.onMessage,
+      this.onError,
+    );
+  }
+
+  onMessage = async (msg: Message, match: RegExpMatchArray) => {
+    let src: string;
+    let dest: string;
+    let text: string;
+
+    if (match[2] != null && match[3] == null) {
+      src = 'auto';
+      dest = match[2].trim();
+    } else {
+      src = (match[2] && 'auto').trim();
+      dest = (match[3] && 'ru').trim();
+    }
+
+    if (src === 'auto' && match[1].toLowerCase() === 'расшифруй') {
+      src = 'ja';
+    }
+
+    if (match[4] !== null) {
+      text = match[4].trim();
+    } else if (msg && msg.reply_to_message && msg.reply_to_message.text) {
+      text = msg.reply_to_message.text;
+    } else {
+      return;
+    }
+
+    const res = await this.translate(src, dest, text);
+    if (res !== null) {
+      this.api.sendMessage({
+        chat_id: msg.chat.id,
+        text: `Перевод: ${res}`,
+      });
+    } else {
+      this.api.sendMessage({
+        chat_id: msg.chat.id,
+        text: 'Сервис недоступен.',
+      });
+    }
+  }
+
+  onError = (msg: Message) => {
+    this.api.sendMessage({
+      chat_id: msg.chat.id,
+      text: 'Не понимаю я эти ваши иероглифы.',
+    });
+  }
+}
