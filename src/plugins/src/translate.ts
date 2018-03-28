66f7338dcdaa4232d73d54905615f0488a40ae36
andretshurotshka <andwebar@gmail.com>
2018-03-28 19:38:08 +0500

    refactor(plugins/translate): remove onError handler

--- src/plugins/src/translate.ts
+++ src/plugins/src/translate.ts
@@ -9,29 +9,33 @@ import { Message } from 'node-telegram-bot-api';
 export class TranslatePlugin implements Plugin {
   readonly name = 'Translate';
 
-  constructor(
-    private api: TgApi,
-    private web: Web,
-  ) {}
+  constructor(private api: TgApi, private web: Web) {}
 
-  async translate(src: string, dest: string, txt: string): Promise<string | null> {
-    const res = await this.web.getAsBrowser('http://translate.google.com/translate_a/single', {
-      qs: {
-        client: 's',
-        ie: 'UTF-8',
-        oe: 'UTF-8',
-        sl: src,
-        tl: dest,
-        dt: 't',
-        q: txt,
+  async translate(
+    src: string,
+    dest: string,
+    txt: string,
+  ): Promise<string | null> {
+    const res = await this.web.getAsBrowser(
+      'http://translate.google.com/translate_a/single',
+      {
+        qs: {
+          client: 's',
+          ie: 'UTF-8',
+          oe: 'UTF-8',
+          sl: src,
+          tl: dest,
+          dt: 't',
+          q: txt,
+        },
       },
-    });
+    );
 
     try {
       console.log(res);
       const evalFn = new Function(`return ${res}`);
       const json: string[][] = evalFn();
-      return json[0].map((d) => d[0]).join('');
+      return json[0].map(d => d[0]).join('');
     } catch (e) {
       console.error(e);
       return null;
@@ -42,46 +46,49 @@ export class TranslatePlugin implements Plugin {
     input.onText(
       /!(переведи|translate|перевод|расшифруй|tr)( [a-z]{2})?( [a-z]{2})?(?: ([^]+))?$/,
       this.onMessage,
-      this.onError,
     );
   }
 
   onMessage = async (msg: Message, match: RegExpMatchArray): Promise<void> => {
-    let src: string;
-    let dest: string;
-    let text: string;
+    try {
+      let src: string;
+      let dest: string;
+      let text: string;
 
-    if (match[2] != null && match[3] == null) {
-      src = 'auto';
-      dest = match[2].trim();
-    } else {
-      src = (match[2] && 'auto').trim();
-      dest = (match[3] && 'ru').trim();
-    }
+      if (match[2] != null && match[3] == null) {
+        src = 'auto';
+        dest = match[2].trim();
+      } else {
+        src = (match[2] && 'auto').trim();
+        dest = (match[3] && 'ru').trim();
+      }
 
-    if (src === 'auto' && match[1].toLowerCase() === 'расшифруй') {
-      src = 'ja';
-    }
+      if (src === 'auto' && match[1].toLowerCase() === 'расшифруй') {
+        src = 'ja';
+      }
 
-    if (match[4] !== null) {
-      text = match[4].trim();
-    } else if (msg && msg.reply_to_message && msg.reply_to_message.text) {
-      text = msg.reply_to_message.text;
-    } else {
-      return;
-    }
+      if (match[4] !== null) {
+        text = match[4].trim();
+      } else if (msg && msg.reply_to_message && msg.reply_to_message.text) {
+        text = msg.reply_to_message.text;
+      } else {
+        return;
+      }
 
-    const res = await this.translate(src, dest, text);
-    if (res !== null) {
-      this.api.sendMessage({
-        chat_id: msg.chat.id,
-        text: `Перевод: ${res}`,
-      });
-    } else {
-      this.api.sendMessage({
-        chat_id: msg.chat.id,
-        text: 'Сервис недоступен.',
-      });
+      const res = await this.translate(src, dest, text);
+      if (res !== null) {
+        this.api.sendMessage({
+          chat_id: msg.chat.id,
+          text: `Перевод: ${res}`,
+        });
+      } else {
+        this.api.sendMessage({
+          chat_id: msg.chat.id,
+          text: 'Сервис недоступен.',
+        });
+      }
+    } catch (e) {
+      this.onError(msg);
     }
   }
 
